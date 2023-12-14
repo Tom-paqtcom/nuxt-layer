f8756104dbcad0a8c217e97fc47e3e045b6e48fc
Tom Stemerding <tom.stemerding@paqt.com>
2023-12-14 12:26:49 +0100

    Update bg-color

--- components/Menu.vue
+++ components/Menu.vue
@@ -3,7 +3,7 @@
 <template>
   Test
   <nav
-    class="block w-full py-2 mx-auto bg-white border shadow-md border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:py-4"
+    class="block w-full py-2 mx-auto bg-black border shadow-md border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:py-4"
   >
     <div
       class="container flex items-center justify-between mx-auto text-blue-gray-900"
